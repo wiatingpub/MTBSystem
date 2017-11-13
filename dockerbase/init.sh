277891e82c31fed441e3793d7809bbe5b861f730
ricoder <1359704355@qq.com>
2017-11-14 01:20:41 +0800

    up 进度修改

--- dockerbase/init.sh
+++ dockerbase/init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# 将supervisor的conf文件复制到指定目录并且重启supervisorctl
+cp /data/deploy/mtbsystem/dockerbase/supervisor/*conf /etc/supervisor/conf.d/
+# 将conf的文件复制到指定目录
+cp /data/deploy/mtbsystem/dockerbase/conf/redis.conf /etc/redis/6379.conf
+mkdir /etc/consul/
+cp /data/deploy/mtbsystem/dockerbase/conf/consul.json /etc/consul/consul.json
+# 重新用supervisor加载进程
+supervisorctl reload
 # 创建数据库和表
 mysql -u root -e "CREATE DATABASE mtbsystem"
 R=/data/deploy/mtbsystem/sql/
